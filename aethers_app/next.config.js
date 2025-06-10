d654b06af04f4ac8c9c30c266a2a6a2576403a90
albertoespejel <42525704+albertoespejel@users.noreply.github.com>
2025-06-09 22:41:13 -0600

    updating for deployment

--- aethers_app/next.config.js
+++ aethers_app/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'export', // <-- THIS replaces `next export`
   };
   
 module.exports = nextConfig;
+  
