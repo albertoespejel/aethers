07b65cc6424f2d47bb57c261c586c3009f8f08d0
albertoespejel <42525704+albertoespejel@users.noreply.github.com>
2025-08-12 23:18:13 -0600

    added link to playlist

--- aethers_app/src/app/page.tsx
+++ aethers_app/src/app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://open.spotify.com/playlist/4yRK3WfyfQOG40tenALKRr?si=25lvx6MUQ6eV4INAr_2AwA"
           target="_blank"
           rel="noopener noreferrer"
         >
